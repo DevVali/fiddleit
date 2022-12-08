555a13e40d741208fbff61daccbd689d92aef18f
ND-debug <117597239+ND-debug@users.noreply.github.com>
2022-12-08 21:48:21 +0100

    Update no.php

--- lang/no.php
+++ lang/no.php
@@ -13,6 +13,6 @@ $text = [
   "privacy" => "Personvern",
   "contact" => "Kontakt",
   "credits" => "Credits",
-  "notTranslated" => "NB: Denne nettsiden er ikke fullstendig oversatt til engelsk.", # Replace 'English' with the language you're writing the translation to
+  "notTranslated" => "NB: Denne nettsiden er ikke fullstendig oversatt til norsk.", # Replace 'English' with the language you're writing the translation to
   "partnership" => "i partnerskap med",
 ];
