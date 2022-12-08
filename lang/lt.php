f3f59ff3642e86889c89f5f38fe2375b19d3173e
dreazn1 <120130260+dreazn1@users.noreply.github.com>
2022-12-08 18:44:38 +0000

    Translated to Lithuanian

---
+++ lang/lt.php
@@ -0,0 +1,18 @@
+<?php
+
+$text = [
+  "title" => "internetinis kodo redaktorius",
+  "loadLabel" => "Įkelti",
+  "loadDescr" => "Įkelti failą iš savo kompiuterio",
+  "runLabel" => "Paleisti",
+  "saveLabel" => "Išsaugoti",
+  "openLabel" => "Atidaryti",
+  "downloadLabel" => "Parsisiųsti",
+  "newTabLabel" => "Atidaryti naujame skirtuke",
+  "copyLabel" => "Kopijuoti",
+  "privacy" => "Privatumas",
+  "contact" => "Kontaktas",
+  "credits" => "Kreditai",
+  "notTranslated" => "Patarimas: šis puslapis nėra visiškai išverstas į anglų kalbą.",
+  "partnership" => "bendradarbiaujant su",
+];
