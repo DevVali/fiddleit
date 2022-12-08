67a95812a9b42e09dc72743d704ec7d2b910c02b
ND-debug <117597239+ND-debug@users.noreply.github.com>
2022-12-08 21:08:55 +0100

    Translated to Norwegian

---
+++ lang/no.php
@@ -0,0 +1,18 @@
+<?php
+
+$text = [
+  "title" => "online kode editor",
+  "loadLabel" => "Last",
+  "loadDescr" => "Last opp filen fra pcen din",
+  "runLabel" => "Kjør",
+  "saveLabel" => "Lagre",
+  "openLabel" => "Åpne",
+  "downloadLabel" => "Last ned",
+  "newTabLabel" => "Åpne i ett nytt vindu",
+  "copyLabel" => "Kopier",
+  "privacy" => "Personvern",
+  "contact" => "Kontakt",
+  "credits" => "Credits",
+  "notTranslated" => "NB: Denne nettsiden er ikke fullstendig oversatt til engelsk.", # Replace 'English' with the language you're writing the translation to
+  "partnership" => "i partnerskap med",
+];
