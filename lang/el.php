950c29aa4cae5853add6aabf164ceb6bfeec8a17
Aggelos-007 <104696548+aggelos-007@users.noreply.github.com>
2022-12-08 21:35:53 +0200

    Create el.php

---
+++ lang/el.php
@@ -0,0 +1,18 @@
+<?php
+
+$text = [
+  "title" => "γράψε κώδικα online",
+  "loadLabel" => "Φώρτωσε",
+  "loadDescr" => "Φώρτωσε αρχεία από τον υπολογιστή σου",
+  "runLabel" => "Τρέξε",
+  "saveLabel" => "Αποθήκευση",
+  "openLabel" => "Άνοιγμα",
+  "downloadLabel" => "Λήψη",
+  "newTabLabel" => "Άνοιγμα σε νέα καρτέλα",
+  "copyLabel" => "Αντιγραφή",
+  "privacy" => "Privacy",
+  "contact" => "Επικεινώνησε",
+  "credits" => "Credits",
+  "notTranslated" => "Σημείωση: αυτή η σελίδα δεν είναι πλήρης μεταφρασμένη στα Ελληνικά.",
+  "partnership" => "σε συνεργασία με",
+];
