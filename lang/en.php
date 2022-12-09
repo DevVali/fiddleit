ccff6109d7272b64656c27e5322f4e436a6144fd
Chris <120158785+LegendaryChriz@users.noreply.github.com>
2022-12-08 21:59:00 -0800

    Update en.php

--- lang/en.php
+++ lang/en.php
@@ -1,18 +1,19 @@
+
 <?php
 
 $text = [
-  "title" => "online code editor",
-  "loadLabel" => "Load",
-  "loadDescr" => "Load file from your computer",
-  "runLabel" => "Run",
-  "saveLabel" => "Save",
-  "openLabel" => "Open",
+  "title" => "editor ng online code",
+  "loadLabel" => "Mag-load",
+  "loadDescr" => "Mag load ng file mula sa iyong computer",
+  "runLabel" => "Tumakbo ka na",
+  "saveLabel" => "Mag-ipon",
+  "openLabel" => "Buksan ang",
   "downloadLabel" => "Download",
-  "newTabLabel" => "Open in a new tab",
-  "copyLabel" => "Copy",
-  "privacy" => "Privacy",
-  "contact" => "Contact",
-  "credits" => "Credits",
-  "notTranslated" => "Note: this page is not fully translated to English.", # Replace 'English' with the language you're writing the translation to
-  "partnership" => "in partnership with",
+  "newTabLabel" => "Buksan sa isang bagong tab",
+  "copyLabel" => "Kopyahin ang",
+  "privacy" => "Pagkapribado",
+  "contact" => "Makipag ugnay sa",
+  "credits" => "Mga Kredito",
+  "notTranslated" => "Note: this page is not fully translated to Tagalog.", # Replace 'English' with the language you're writing the translation to
+  "partnership" => "Sa pakikipagtulungan Sa",
 ];
