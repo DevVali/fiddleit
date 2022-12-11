3f6ba8c6bf5f3e9a176b9642b9a3d1e3126a218b
kipteam <87072972+ToroEen@users.noreply.github.com>
2022-12-11 20:19:29 +0100

    Added Dutch translations

---
+++ lang/nl.php
@@ -0,0 +1,18 @@
+<?php
+
+$text = [
+  "title" => "online code-editor",
+  "loadLabel" => "Laden",
+  "loadDescr" => "Laad vanaf je computer",
+  "runLabel" => "Uitvoeren",
+  "saveLabel" => "Opslaan",
+  "openLabel" => "Open",
+  "downloadLabel" => "Download",
+  "newTabLabel" => "Openen in een nieuw tablad",
+  "copyLabel" => "Kopiëren",
+  "privacy" => "Privacy",
+  "contact" => "Contacteren",
+  "credits" => "Credits",
+  "notTranslated" => "PS: deze pagina is niet volledig vertaal naar het Nederlands.", # Replace 'English' with the language you're writing the translation to
+  "partnership" => "in samenwerking met",
+];
